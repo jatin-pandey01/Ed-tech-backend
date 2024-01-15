6fd12f31e7b73af46656f4533e4afc6c7ed8c6b3
jatin-pandey01 <=>
2024-01-15 12:39:41 +0530

    Gitignore update

--- routes/Profile.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-//Import required controllers and middleware functions
-const {updateProfile, deleteAccount, getAllUserDetails} = require('../controllers/Profile');
